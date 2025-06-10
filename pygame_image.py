56d382609618527f0c487702b1c43b568c8e9482
hirose <c0a2421241@edu.teu.ac.jp>
2025-06-10 15:42:00 +0900

    練習7

--- pygame_image.py
+++ pygame_image.py
@@ -17,6 +17,7 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         screen.blit(bg_img, [-tmr, 0])
+        screen.blit(bg_img, [-tmr+1600, 0])
         screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
